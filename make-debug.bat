c7e6de26891dfbaf21ffd6ce7975d843724096a0
Henry Slawniak <meggawatts@gmail.com>
2013-11-13 00:51:36 -0600

    Sound files now provided by another submodule

--- make-debug.bat
+++ make-debug.bat
@@ -3,7 +3,9 @@ cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
 if not exist %newDir%\Rohan-DEBUG mkdir %newDir%\Rohan-DEBUG
+del %newdir%\Rohan-DEBUG\*.* /s /q
 xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Rohan-DEBUG
 xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Rohan-DEBUG\assets\minecraft
 xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
 pause
