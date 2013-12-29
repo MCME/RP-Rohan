c227861364f825ee33c64eb48cc180b5e798e371
Henry Slawniak <meggawatts@gmail.com>
2013-12-28 23:52:19 -0600

    Revert "Output to different dir"
    This reverts commit 47bd02f582751f9b6bb810aaa6ea57a7b3faaff4.

--- make-debug.bat
+++ make-debug.bat
@@ -2,9 +2,9 @@ SET oldDir=%CD%
 cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
-if not exist %newDir%\Rohan-DEBUG-BIOMES mkdir %newDir%\Rohan-DEBUG-BIOMES
-del %newdir%\Rohan-DEBUG-BIOMES\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Rohan-DEBUG-BIOMES
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Rohan-DEBUG-BIOMES\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Rohan-DEBUG-BIOMES\assets\minecraft
-xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Rohan-DEBUG-BIOMES\assets\minecraft
+if not exist %newDir%\Rohan-DEBUG mkdir %newDir%\Rohan-DEBUG
+del %newdir%\Rohan-DEBUG\*.* /s /q
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Rohan-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Rohan-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
