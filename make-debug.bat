87eaa23f4f1ed0fe4c6ef6660e642a4b9ececda9
Henry Slawniak <meggawatts@gmail.com>
2014-04-19 13:34:52 -0500

    Update debug script

--- make-debug.bat
+++ make-debug.bat
@@ -2,9 +2,9 @@ SET oldDir=%CD%
 cd %appdata%\.minecraft\resourcepacks
 SET newDir=%CD%
 cd %oldDir%
-if not exist %newDir%\Rohan-DEBUG mkdir %newDir%\Rohan-DEBUG
-del %newdir%\Rohan-DEBUG\*.* /s /q
-xcopy /y/s %oldDir%\src\main\meta\* %newDir%\Rohan-DEBUG
-xcopy /y/s %oldDir%\src\main\pack\* %newDir%\Rohan-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
-xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\Rohan-DEBUG\assets\minecraft
+if not exist %newDir%\MCME-Rohan-DEBUG mkdir %newDir%\MCME-Rohan-DEBUG
+del %newdir%\MCME-Rohan-DEBUG\*.* /s /q
+xcopy /y/s %oldDir%\src\main\meta\* %newDir%\MCME-Rohan-DEBUG
+xcopy /y/s %oldDir%\src\main\pack\* %newDir%\MCME-Rohan-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-UI\src\main\pack\*.* %newDir%\MCME-Rohan-DEBUG\assets\minecraft
+xcopy /y/s %oldDir%\MCME-Aural-Experience\src\main\pack\*.* %newDir%\MCME-Rohan-DEBUG\assets\minecraft
